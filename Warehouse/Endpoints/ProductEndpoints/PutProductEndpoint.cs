1e76ef0770b2aa686f04cf3ba219bd4a53db1294
Владислав Маршин <meat-marshin@yandex.ru>
2023-01-19 09:34:44 +0300

    change put product logic

--- Warehouse/Endpoints/ProductEndpoints/PutProductEndpoint.cs
+++ Warehouse/Endpoints/ProductEndpoints/PutProductEndpoint.cs
@@ -42,9 +42,10 @@ namespace Warehouse.Endpoints.ProductEndpoints
                 if (departmentDBWithDetails == null)
                 {
                     await SendStringAsync("No such department in the database.", statusCode: 404, cancellation: ct);
-                    productDBWithDetails!.Department = departmentDBWithDetails;
                     return;
                 }
+                productDBWithDetails!.Department = departmentDBWithDetails;
+                productDBWithDetails!.DepartmentId = departmentDBWithDetails!.Id;
             }
 
             productDBWithDetails = Map.UpdateEntity(product, productDBWithDetails!);
